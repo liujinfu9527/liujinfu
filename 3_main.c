065b5a7deb119ac7cda6761da80c5b2d111f59c7
liujinfu9527 <2151641135@qq.com>
2025-10-21 21:06:40 +0800

    Update mian .c

---
+++ 3_main.c
@@ -0,0 +1,17 @@
+//202510302201
+//2151641135
+//刘金富
+#include <stdio.h>
+int main() {
+    float a,b,c;
+    scanf("%f,%f",&a,&b);
+    c=a+b;
+    printf("a=%f\n",a);
+    printf("b=%f\n",b);
+    printf("a+b=%f\n",c);
+    return 0;
+
+
+
+}
+ 
