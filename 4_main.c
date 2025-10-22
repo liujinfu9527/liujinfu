923bab871f4fff8967902203acde3c1f54756ced
liujinfu9527 <2151641135@qq.com>
2025-10-22 16:09:33 +0800

    Update mian .c

--- 4_main.c
+++ 4_main.c
@@ -5,7 +5,7 @@
 int main() {
    int a,b,c;
    printf("请输入三角形的三边长，以逗号分隔：");
-    scanf("%d,%d,%d",&a,&b,&c);
+    scanf ("%d %d %d",&a,&b,&c);
     if(a+b>c && a+c>b && b+c>a){
         printf("可以构成三角形\n");
     }else{
