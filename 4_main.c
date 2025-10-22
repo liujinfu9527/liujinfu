841c2d91c08f4a9fa2ecafe1fc25911eb6f5c8fd
liujinfu9527 <2151641135@qq.com>
2025-10-22 16:17:18 +0800

    Update mian .c

--- 4_main.c
+++ 4_main.c
@@ -4,12 +4,12 @@
 #include <stdio.h>
 int main() {
    int a,b,c;
-   printf("代表三条线段长度：");
+   printf("输入三个整数（代表三条线段长度）：");
     scanf("%d,%d,%d",&a,&b,&c);
     if(a+b>c && a+c>b && b+c>a){
-        printf("可以构成三角形\n");
+        printf("可以组成三角形\n");
     }else{
-        printf("不可以构成三角形\n");
+        printf("不能组成三角形\n");
     }
         return 0;
 }
