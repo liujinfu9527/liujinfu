7b83c2c0e73ec7766c9acea7b9516e8aaa1c9e29
liujinfu9527 <2151641135@qq.com>
2025-10-22 15:58:27 +0800

    Update mian .c

---
+++ 4_main.c
@@ -0,0 +1,14 @@
+//202510302201
+//2151641135@qq.com
+//刘金富
+#include <stdio.h>
+int main() {
+   int a,b,c;
+    scanf ("%d,%d,%d",&a,&b,&c);
+    if(a+b>c && a+c>b && b+c>a){
+        printf("可以构成三角形\n");
+    }else{
+        printf("不可以构成三角形\n");
+    }
+        return 0;
+}
