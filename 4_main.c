b23709164a52d6ac48875ae1175408907aa44817
liujinfu9527 <2151641135@qq.com>
2025-10-27 17:21:00 +0800

    Update mian .c

--- 4_main.c
+++ 4_main.c
@@ -2,14 +2,25 @@
 //2151641135@qq.com
 //刘金富
 #include <stdio.h>
-int main() {
-   int a,b,c;
-   printf("输入三个整数（代表三条线段长度）：");
-    scanf("%d,%d,%d",&a,&b,&c);
-    if(a+b>c && a+c>b && b+c>a){
-        printf("可以组成三角形\n");
-    }else{
-        printf("不能组成三角形\n");
-    }
-        return 0;
+int main () 
+{
+    int first =1 ;
+    for (int i = 100; i<= 999; i++) 
+{
+    int a=i / 100;
+    int b=(i /10) % 10;
+    int c=i % 10;
+    if (a*a*a + b*b*b + c*c*c == i)
+{
+    if (first){
+    printf("%d", i);
+    first =0;
+}
+    else
+{
+    printf(" %d", i);
+}
+}
+}
+    return 0;
 }
