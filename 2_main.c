0d70622959cbf3a3d20c14d4b5c530a5497ee173
liujinfu9527 <2151641135@qq.com>
2025-10-27 17:09:16 +0800

    Update mian .c

--- 2_main.c
+++ 2_main.c
@@ -2,23 +2,25 @@
 //2151641135@qq.com
 //刘金富
 #include <stdio.h>
-int main(){
-    int score;
-    printf("请输入学生成绩（0-100）：");
-    scanf("%d", &score);
-    if (score >= 90 && score <= 100) {
-        printf("A\n");
-    } else if (score >= 80 && score < 90) {
-        printf("B\n");
-    } else if (score >= 70 && score < 80) {
-        printf("C\n");
-    } else if (score >= 60 && score < 70) {
-        printf("D\n");
-    } else if (score >= 0 && score < 60) {
-        printf("E\n");
-    } else {
-        printf("输入的成绩无效，请输入0到100之间的数字!\n");
-    }
-    
+int main () 
+{
+    int first =1 ;
+    for (int i = 100; i<= 999; i++) 
+{
+    int a=i / 100;
+    int b=(i /10) % 10;
+    int c=i % 10;
+    if (a*a*a + b*b*b + c*c*c == i)
+{
+    if (first)
+{
+    printf("%d", i);
+    first =0;
+}      else
+{
+    printf(" %d", i);
+}
+}
+}
     return 0;
 }
