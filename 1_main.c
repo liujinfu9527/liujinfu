64b1133521dc5460c6bf310b90b566cb0399f59a
liujinfu9527 <2151641135@qq.com>
2025-10-27 16:47:48 +0800

    Update mian .c

--- 1_main.c
+++ 1_main.c
@@ -2,10 +2,30 @@
 //2151641135@qq.com
 //刘金富
 #include <stdio.h>
-int main() {
-    float celsius,fahrenheit;
-    scanf("%f",&celsius);
-    fahrenheit = celsius * 9 / 5 + 32;
-    printf("%.1f\n",fahrenheit);
-    return 0;
+int main () 
+{
+   int n;
+   scanf("%d", &n);
+   int is_prime=1;
+   if(n >=50)
+   return 0;
+   if(n==1){
+    is_prime=0;
+}  else    {
+   
+   for (int i = 2;i <n;i++){
+   if(n % i == 0){
+   is_prime = 0;
+   break;
 }
+}
+}
+    if (is_prime){
+        printf("密钥安全，密码设置成功\n");
+}else      {
+        printf("密钥不安全，请重新输入\n");
+}
+     return 0;   
+}
+    
+    
