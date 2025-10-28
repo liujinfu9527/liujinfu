21ea550adf5cc619ff648dd4a9966f5e0e080ba3
liujinfu9527 <2151641135@qq.com>
2025-10-28 15:48:48 +0800

    Update mian .c

--- 3_main.c
+++ 3_main.c
@@ -2,11 +2,40 @@
 //2151641135@qq.com
 //刘金富
 #include <stdio.h>
-int main() {
-    float a,b,c;
-    scanf ("%f %f",&a,&b);
-    c=a+b;
-    printf("%f + %f = %f\n",a,b,c);
-    return 0;
+int main () 
+{
+    int n;
+    scanf("%d", &n);
+    int is_prime = 1;
+    if(n ==1)
+{
+    is_prime = 0;
 }
- 
+    else
+{
+    int i = 2;
+    while(i * i <= n)
+{
+    if(n % i == 0)
+{
+    is_prime = 0;
+    break;
+}
+            i++;
+}
+}
+    if(is_prime)
+{
+    printf("密钥安全，密码设置成功\n");
+}
+    else
+{
+    printf("密钥不安全，请重新输入\n");
+}
+    return 0;
+
+
+
+
+
+}
