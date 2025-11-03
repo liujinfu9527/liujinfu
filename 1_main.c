6193943b2dca88394f0a0b6f21c8bab0a7b3bb54
liujinfu9527 <2151641135@qq.com>
2025-11-03 14:34:00 +0800

    Update mian .c

--- 1_main.c
+++ 1_main.c
@@ -5,18 +5,17 @@
 int main (){
 int arr[3][3];
 int i,j;
-for (i=0;i<3;i++)
+    for (i=0;i<3;i++)
 {
-scanf ("%d %d %d",&arr[i][0],&arr[i][1],&arr[i][2]);
+    scanf ("%d %d %d",&arr[i][0],&arr[i][1],&arr[i][2]);
 }
-for (i=0;i<3;i++){
-for (j=0;j<3;j++){
-printf ("%d ",arr[i][j]);
-if(j < 2) printf (" ");
+    for (i=0;i<3;i++){
+    for (j=0;j<3;j++){
+    printf ("%d",arr[i][j]);
+    if(j < 2) {printf (" ");
 }
-printf ("\n");
+    printf ("\n");
 }
-
-return 0;
+    return 0;
 }
 
