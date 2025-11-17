d6d94c92db043e026e3db16f9d6360f9d0cac088
liujinfu9527 <2151641135@qq.com>
2025-11-17 12:51:39 +0800

    Update mian .c

--- 1_main.c
+++ 1_main.c
@@ -2,20 +2,12 @@
 //2151641135@qq.com
 //刘金富
 #include <stdio.h>
-int main (){
-int arr[3][3];
-int i,j;
-    for (i=0;i<3;i++)
+int main ()
 {
-    scanf ("%d %d %d",&arr[i][0],&arr[i][1],&arr[i][2]);
-}
-    for (i=0;i<3;i++){
-    for (j=0;j<3;j++){
-    printf ("%d",arr[i][j]);
-    if(j < 2) printf (" ");
-}
-    printf ("\n");
-}
+    int a;
+    scanf ("%d",a);
+    int *ptr = &a;
+    *ptr += 10;
+    printf ("%d,%d" ,a,*ptr);
     return 0;
 }
-
