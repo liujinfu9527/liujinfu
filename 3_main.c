d931e6a99f2ee8b43b49ac4e8d996429dd34ad5e
liujinfu9527 <2151641135@qq.com>
2025-11-17 13:02:45 +0800

    Update mian .c

--- 3_main.c
+++ 3_main.c
@@ -2,29 +2,18 @@
 //2151641135@qq.com
 //刘金富
 #include <stdio.h>
+void swap (int *a,int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main ()
 {
-int arr[10];
-int i,j;
-    for (i = 0;i < 10;i++)
-{
-    scanf ("%d",&arr[i]);
-}
-    for (i = 0;i < 9;i++)
-{
-    for (j = 0;j < 9;j++)
-{
-    if (arr[j] > arr[j+1])
-{
-    arr[j] = arr[j] + arr[j+1];
-    arr[j+1] = arr[j] - arr[j+1];
-    arr[j] = arr[j] - arr[j+1];
-}
-}
-}
-    for(i = 0;i < 10;i++)
-{
-    printf("%d ",arr[i]);
-}
+    int c,d;
+    scanf ("%d %d",&c,&d);
+    swap(&c,&d);
+    printf ("%d %d",c,d);
     return 0;
+    
 }
